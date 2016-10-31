aae0cf3eb0f04d9d9c07db47770985f5ee321e41
OCZ\sam.gonshaw <sam.gonshaw@taec.toshiba.com>
2016-10-31 16:50:33 +0000

    Added basic makefile

---
+++ include/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define MAIN_INTRODUCTION printf("Hello world.\n")
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(int argc, char ** argv) {
+    MAIN_INTRODUCTION;
+    return 0;
+}
